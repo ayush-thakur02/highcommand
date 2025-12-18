3671662731cbd816aebf9590e7a1468d3c27be13
ayush-thakur02 <ayush.th2002@gmail.com>
2025-12-18 15:06:47 +0530

    feat: Add project detail page and update server listening port to 5001.

--- web_app.py
+++ web_app.py
@@ -620,4 +620,4 @@ if __name__ == '__main__':
     print("="*70)
     print("\n  Server starting at: http://localhost:5000")
     print("  Press CTRL+C to quit\n")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
